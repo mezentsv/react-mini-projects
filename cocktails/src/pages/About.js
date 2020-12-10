4e0736956c763756bfc4a7d0842fc90895e583da
Artem Mezentsev <mezentsv@gmail.com>
2020-12-10 09:17:03 +0300

    Finish Cocktaild project

---
+++ cocktails/src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto libero
+        quaerat est quidem ab earum sequi vel adipisci, repellat voluptates
+        deserunt sit ad quos minus hic natus. Voluptatum dicta iure, unde
+        maiores laboriosam cupiditate quae maxime voluptas sequi, animi nostrum.
+      </p>
+    </section>
+  );
+};
+
+export default About;
